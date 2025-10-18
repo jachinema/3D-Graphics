5a98f9bb55184ddc235ec4e9268c0ed92938bc0b
jachinema <99571020+jachinema@users.noreply.github.com>
2025-10-18 00:50:11 -0500

    Blank slate

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+import pygame 
+
+setup(
+    name='3DGraphics-jachinema',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'pygame'
+    ]
+)
